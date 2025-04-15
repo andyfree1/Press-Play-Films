dc95f8f79e991876b98e1c671549cee03c986bfb
Andrew Freeman <andyfree@gmail.com>
2025-04-15 01:43:22 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
-// src/index.ts
-export * from "@tanstack/query-core";
-export * from "./types.js";
-import { useQueries } from "./useQueries.js";
-import { useQuery } from "./useQuery.js";
-import { useSuspenseQuery } from "./useSuspenseQuery.js";
-import { useSuspenseInfiniteQuery } from "./useSuspenseInfiniteQuery.js";
-import { useSuspenseQueries } from "./useSuspenseQueries.js";
-import { usePrefetchQuery } from "./usePrefetchQuery.js";
-import { usePrefetchInfiniteQuery } from "./usePrefetchInfiniteQuery.js";
-import { queryOptions } from "./queryOptions.js";
-import { infiniteQueryOptions } from "./infiniteQueryOptions.js";
-import {
-  QueryClientContext,
-  QueryClientProvider,
-  useQueryClient
-} from "./QueryClientProvider.js";
-import { HydrationBoundary } from "./HydrationBoundary.js";
-import {
-  QueryErrorResetBoundary,
-  useQueryErrorResetBoundary
-} from "./QueryErrorResetBoundary.js";
-import { useIsFetching } from "./useIsFetching.js";
-import { useIsMutating, useMutationState } from "./useMutationState.js";
-import { useMutation } from "./useMutation.js";
-import { useInfiniteQuery } from "./useInfiniteQuery.js";
-import { useIsRestoring, IsRestoringProvider } from "./isRestoring.js";
-export {
-  HydrationBoundary,
-  IsRestoringProvider,
-  QueryClientContext,
-  QueryClientProvider,
-  QueryErrorResetBoundary,
-  infiniteQueryOptions,
-  queryOptions,
-  useInfiniteQuery,
-  useIsFetching,
-  useIsMutating,
-  useIsRestoring,
-  useMutation,
-  useMutationState,
-  usePrefetchInfiniteQuery,
-  usePrefetchQuery,
-  useQueries,
-  useQuery,
-  useQueryClient,
-  useQueryErrorResetBoundary,
-  useSuspenseInfiniteQuery,
-  useSuspenseQueries,
-  useSuspenseQuery
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const all_1 = __importDefault(require("./configs/all"));
+const base_1 = __importDefault(require("./configs/base"));
+const disable_type_checked_1 = __importDefault(require("./configs/disable-type-checked"));
+const eslint_recommended_1 = __importDefault(require("./configs/eslint-recommended"));
+const recommended_1 = __importDefault(require("./configs/recommended"));
+const recommended_type_checked_1 = __importDefault(require("./configs/recommended-type-checked"));
+const recommended_type_checked_only_1 = __importDefault(require("./configs/recommended-type-checked-only"));
+const strict_1 = __importDefault(require("./configs/strict"));
+const strict_type_checked_1 = __importDefault(require("./configs/strict-type-checked"));
+const strict_type_checked_only_1 = __importDefault(require("./configs/strict-type-checked-only"));
+const stylistic_1 = __importDefault(require("./configs/stylistic"));
+const stylistic_type_checked_1 = __importDefault(require("./configs/stylistic-type-checked"));
+const stylistic_type_checked_only_1 = __importDefault(require("./configs/stylistic-type-checked-only"));
+const rules_1 = __importDefault(require("./rules"));
+// note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
+const { name, version } = require('../package.json');
+const configs = {
+    all: all_1.default,
+    base: base_1.default,
+    'disable-type-checked': disable_type_checked_1.default,
+    'eslint-recommended': eslint_recommended_1.default,
+    recommended: recommended_1.default,
+    /** @deprecated - please use "recommended-type-checked" instead. */
+    'recommended-requiring-type-checking': recommended_type_checked_1.default,
+    'recommended-type-checked': recommended_type_checked_1.default,
+    'recommended-type-checked-only': recommended_type_checked_only_1.default,
+    strict: strict_1.default,
+    'strict-type-checked': strict_type_checked_1.default,
+    'strict-type-checked-only': strict_type_checked_only_1.default,
+    stylistic: stylistic_1.default,
+    'stylistic-type-checked': stylistic_type_checked_1.default,
+    'stylistic-type-checked-only': stylistic_type_checked_only_1.default,
+};
+const meta = {
+    name,
+    version,
+};
+module.exports = {
+    configs,
+    meta,
+    rules: rules_1.default,
 };
 //# sourceMappingURL=index.js.map
