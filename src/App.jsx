8eed21147fac9683ce52a1dd4b30cff006a0e65d
Andrew Freeman <andyfree@gmail.com>
2025-04-14 23:48:49 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Press Play Films</h1>
+      <p>Website coming soon!</p>
+    </div>
+  )
+}
+
+export default App
